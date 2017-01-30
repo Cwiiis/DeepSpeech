c6387c8485fce3af9a23079fed31e851333f5a9a
Chris Lord <chrislord.net@gmail.com>
2017-01-30 16:28:48 +0000

    Add SOX code to decode/resample/downmix audio

--- native_client/client.cc
+++ native_client/client.cc
@@ -1,47 +1,104 @@
-#include <iostream>
+#include <assert.h>
 #include <sox.h>
 #include "deepspeech.h"
+#include "kiss_fft.h"
+#include "libmfcc.h"
 
 int main(int argc, char **argv)
 {
+  const size_t win_size = 512;
+
   if (argc < 3) {
-    std::cout << "Usage: deepspeech [model path] [audio path]\n";
+    printf("Usage: deepspeech [model path] [audio path]\n");
     return 1;
   }
 
   // Initialise DeepSpeech
   DeepSpeechContext* ctx = DsInit(argv[1]);
-  if (!ctx) {
-    std::cout << "Error initialising DeepSpeech.\n";
-    return 1;
-  }
+  assert(ctx);
 
   // Initialise SOX
-  int sox_return_code = sox_format_init();
-  if (sox_return_code != 0) {
-    std::cout << "Error initialising SoX\n";
-    return 1;
-  }
+  assert(sox_init() == SOX_SUCCESS);
 
-  sox_format_t* audio_file = sox_open_read(argv[2], NULL, NULL, NULL);
-  if (audio_file) {
-    sox_sample_t *buffer =
-      static_cast<sox_sample_t *>(malloc(sizeof(sox_sample_t) * audio_file->olength));
-    size_t samples = sox_read(audio_file, buffer, audio_file->olength);
+  sox_format_t* input = sox_open_read(argv[2], NULL, NULL, NULL);
+  assert(input);
 
-    if (samples > 0) {
-      
-    } else {
-      std::cout << "Error reading audio file\n";
-    }
+  // Resample/reformat the audio so we can pass it through the MFCC functions
+  sox_signalinfo_t target_signal = {
+      16000, // Rate
+      1, // Channels
+      16, // Precision
+      SOX_UNSPEC, // Length
+      NULL // Effects headroom multiplier
+  };
 
-    free(buffer);
-  } else {
-    std::cout << "Error opening audio file\n";
-  }
+  sox_encodinginfo_t target_encoding = {
+    SOX_ENCODING_UNSIGNED, // Sample format
+    16, // Bits per sample
+    0.0, // Compression factor
+    sox_option_default, // Should bytes be reversed
+    sox_option_default, // Should nibbles be reversed
+    sox_option_default, // Should bits be reversed (?!)
+    sox_false // Reverse endianness
+  };
 
+  char *buffer;
+  size_t buffer_size;
+  sox_format_t* output = sox_open_memstream_write(&buffer, &buffer_size,
+                                                  &target_signal,
+                                                  &target_encoding,
+                                                  "raw", NULL);
+  assert(output);
+
+  // Setup the effects chain to decode/resample
+  char* sox_args[10];
+  sox_effects_chain_t* chain =
+    sox_create_effects_chain(&input->encoding, &output->encoding);
+
+  sox_effect_t* e = sox_create_effect(sox_find_effect("input"));
+  sox_args[0] = (char*)input;
+  assert(sox_effect_options(e, 1, sox_args) == SOX_SUCCESS);
+  assert(sox_add_effect(chain, e, &input->signal, &input->signal) ==
+         SOX_SUCCESS);
+  free(e);
+
+  e = sox_create_effect(sox_find_effect("rate"));
+  assert(sox_effect_options(e, 0, NULL) == SOX_SUCCESS);
+  assert(sox_add_effect(chain, e, &input->signal, &output->signal) ==
+         SOX_SUCCESS);
+  free(e);
+
+  e = sox_create_effect(sox_find_effect("channels"));
+  assert(sox_effect_options(e, 0, NULL) == SOX_SUCCESS);
+  assert(sox_add_effect(chain, e, &input->signal, &output->signal) ==
+         SOX_SUCCESS);
+  free(e);
+
+  e = sox_create_effect(sox_find_effect("output"));
+  sox_args[0] = (char*)output;
+  assert(sox_effect_options(e, 1, sox_args) == SOX_SUCCESS);
+  assert(sox_add_effect(chain, e, &input->signal, &output->signal) ==
+         SOX_SUCCESS);
+  free(e);
+
+  // Finally run the effects chain
+  sox_flow_effects(chain, NULL, NULL);
+  sox_delete_effects_chain(chain);
+  sox_close(output);
+
+  // Run buffer through FFT
+  // ...
+  free(buffer);
+
+  // Run FFT through MFCC
+  // ...
+
+  // Pass MFCC to DeepSpeech
+  // ...
+
+  // Deinitialise and quit
   DsClose(ctx);
-  sox_format_quit();
+  sox_quit();
 
   return 0;
 }
