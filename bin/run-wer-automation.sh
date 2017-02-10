4202c7338a92c75352d0c9587c4d608510094544
Chris Lord <chrislord.net@gmail.com>
2017-02-10 18:09:17 +0000

    Test dropout=0.25, RNN dropout=0.1

--- bin/run-wer-automation.sh
+++ bin/run-wer-automation.sh
@@ -40,8 +40,18 @@ export ds_display_step
 ds_validation_step=10
 export ds_validation_step
 
-ds_dropout_rate=0.275
+ds_dropout_rate=0.25
 export ds_dropout_rate
+
+# Dropout lin(0.2,0.4)
+#export ds_dropout_rate=0.2
+#export ds_dropout_rate2=0.267
+#export ds_dropout_rate3=0.333
+#export ds_dropout_rate6=0.4
+
+# RNN dropout
+export ds_dropout_rate4=0.1
+export ds_dropout_rate5=0.1
 
 ds_default_stddev=0.046875
 export ds_default_stddev
